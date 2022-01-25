0df1515b7117ec00637fcf39a060a46c532ab49a
Matthew Whiteker <mattwhiteker@gmail.com>
2022-01-25 14:33:57 -0500

    Inital Commit

---
+++ WiredBrainCoffee.SpecialCases/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WiredBrainCoffee.SpecialCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = new Container<string>();
+            _ = new Container<string>();
+            _ = new Container<int>();
+
+            Console.WriteLine($"Container<string>: {Container<string>.InstanceCount}");
+            Console.WriteLine($"Container<int>: {Container<int>.InstanceCount}");
+            Console.WriteLine($"Container<bool>: {Container<bool>.InstanceCount}");
+            Console.WriteLine($"ContainerBase: {ContainerBase.InstanceCountBase}");
+
+            Console.ReadLine();
+        }
+    }
+    class ContainerBase
+    {
+        public ContainerBase() => InstanceCountBase++;
+
+        public static int InstanceCountBase { get; private set; }
+    }
+    class Container<T> : ContainerBase
+    {
+        public Container() =>  InstanceCount++;
+
+        public static int InstanceCount { get; private set; }
+    }
+
+  
+   
+}
